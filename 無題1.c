34498d220d7a42339710ecc7824f4dce38357e49
rakuyo <shbloodborne@gmail.com>
2025-10-24 19:20:32 +0900

    Add files via upload

---
+++ 無題1.c
@@ -0,0 +1,12 @@
+#include<float.h>
+#include<limits.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("double maximum:%g\n",DBL_MAX);
+    printf("double minimum:%g\n",DBL_MIN);
+    printf("double precision:%g\n",DBL_DIG);
+
+    return 0;
+}
